69da98c511c9a1e2ef569eab8e12460c17feb2cd
Ansumanbhujabal <ansumanbhujabal1@gmail.com>
2025-01-02 10:34:32 +0530

    Reflection_Agent add

---
+++ Reflection_Agent/app.py
@@ -0,0 +1,55 @@
+from chains import reflection_chain,generation_chain
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+from typing import Sequence, List
+
+
+
+REFLECT="reflect"
+GENERATE="generate"
+
+def generation_node( state:Sequence[BaseMessage] ):
+    return generation_chain.invoke({"messages":state})
+
+def reflection_node(messages:Sequence[BaseMessage])->List[BaseMessage]:
+    res=reflection_chain.invoke({"messages":messages})
+    return[HumanMessage(content=res.content)]
+
+
+
+builder=MessageGraph()
+builder.add_node(GENERATE,generation_node)
+builder.add_node(REFLECT,reflection_node)
+builder.set_entry_point(GENERATE)
+def should_continue(state:List[BaseMessage]):
+    if len(state)>6:
+        return END
+    else:
+        return REFLECT
+
+
+builder.add_conditional_edges(GENERATE,should_continue)
+builder.add_edge(REFLECT,GENERATE)
+
+graph=builder.compile()
+print(graph.get_graph().draw_mermaid_png( output_file_path="graph.png"))
+graph.get_graph().print_ascii()
+
+
+
+
+
+if __name__== "__main__":
+    print("Execution started")
+    input=HumanMessage(content="""
+My new course, Generative AI for Everyone, is now available!
+Learn how Generative AI works, how to use it in professional or personal settings,
+and how it will affect jobs, businesses and society.
+This course is accessible to everyone, and assumes no prior coding or AI experience.""")
+    response=graph.invoke(input)
+    print(response)
+  
+# Unlock the power of Generative AI! My new course, Generative AI for Everyone, is now available!
+# Learn the basics, apply it in your life & profession, and discover how it will shape the future of work & society.
+# No prior coding or AI experience needed! 
+# #GenerativeAI #AIforAll"\n\nLet me know if this one hits the mark or if you\'d like me to try again!'
